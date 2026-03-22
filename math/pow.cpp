7250af1b81c864faca0427b0054f82f940896020
yd-tw <yd960528@gmail.com>
2026-03-23 00:39:04 +0800

    update

---
+++ math/pow.cpp
@@ -0,0 +1,41 @@
+	int ksm(int x, int y) {
+    int ans = 1;              
+    while (y) {             
+        if (y & 1)
+            ans = ans * x;   
+        x = x * x;           
+        y >>= 1;            
+    }
+    return ans;
+}
+
+// 矩陣乘法
+vector<vector<int>> mul(const vector<vector<int>> &A,
+                        const vector<vector<int>> &B) {
+    int n = A.size();
+    vector<vector<int>> C(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) if (A[i][k]) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % MOD;
+            }
+        }
+    }
+    return C;
+}
+
+// 矩陣快速冪
+vector<vector<int>> ksm(vector<vector<int>> base, int exp) {
+    int n = base.size();
+    vector<vector<int>> res(n, vector<int>(n, 0));
+
+    for (int i = 0; i < n; i++) res[i][i] = 1;
+
+    while (exp > 0) {
+        if (exp & 1) res = mul(res, base);
+        base = mul(base, base);
+        exp >>= 1;
+    }
+    return res;
+}
